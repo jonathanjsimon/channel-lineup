3ac86b3d4b14b57f6e160d61bba589b0d9273569
Jonathan Simon <simon.jonathan@gmail.com>
2018-01-14 23:12:29 -0500

    Build tweaks

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ TARGET="build/"
 mkdir -p "${TARGET}"
 rm -rf "${TARGET}/*"
 
-mkdir -p "${TARGET}/{js,css,data}"
+mkdir -p "${TARGET}"/{js,css,data}
 
 html-minifier index.html                 --collapse-whitespace --minify-js --minify-css --o "${TARGET}/index.html" > /dev/null
 
-uglifyjs "/js/index.js"                  -o                                                 "${TARGET}/js/index.js"
-cp       "/js/*.min.js"                                                                     "${TARGET}/js/"
+uglifyjs js/index.js                  -o                                                 "${TARGET}/js/index.js"
+cp       js/*.min.js                                                                     "${TARGET}/js/"
 
-sass     "/css/style.scss"                                                                  "${TARGET}/css/style.css"
+sass     css/style.scss                                                                  "${TARGET}/css/style.css"
 
-cp       "/data/data.json"                                                                  "${TARGET}/data/data.json"
+cp       data/data.json                                                                  "${TARGET}/data/data.json"
