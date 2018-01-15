50a27ae9fb70757ba2a0f9248340615a0d81890e
Jonathan Simon <simon.jonathan@gmail.com>
2018-01-14 20:22:50 -0500

    build script and cleanup

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET="build/"
+
+
+mkdir -p "${TARGET}"
+rm -rf "${TARGET}/*"
+
+mkdir -p "${TARGET}/{js,css,data}"
+
+html-minifier index.html                 --collapse-whitespace --minify-js --minify-css --o "${TARGET}/index.html" > /dev/null
+
+uglifyjs "/js/index.js"                  -o                                                 "${TARGET}/js/index.js"
+cp       "/js/*.min.js"                                                                     "${TARGET}/js/"
+
+sass     "/css/style.scss"                                                                  "${TARGET}/css/style.css"
+
+cp       "/data/data.json"                                                                  "${TARGET}/data/data.json"
